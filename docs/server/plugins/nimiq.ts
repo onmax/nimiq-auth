fc102dfe0570ef4d72085a5401bb8e10411971f6
onmax <maximogarciamtnez@gmail.com>
2025-02-10 09:39:00 +0100

    chore:

--- docs/server/plugins/nimiq.ts
+++ docs/server/plugins/nimiq.ts
@@ -3,7 +3,5 @@ import init from '@nimiq/core/web'
 export default defineNitroPlugin(async () => {
   if (!import.meta.env.PROD)
     return
-  // @ts-expect-error We copy at build time
-  const mod = await import('./nimiq.wasm')
-  init(mod)
+  init('./nimiq.wasm')
 })
